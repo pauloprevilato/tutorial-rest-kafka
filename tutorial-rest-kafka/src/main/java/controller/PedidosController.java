94b5f4ede3c807b3dc4cc3ba8e9fd2c4729456b6
Paulo Previlato <paulo.previlato@gmail.com>
2023-10-16 16:59:35 -0400

    Create Post to salva-pedido.

--- tutorial-rest-kafka/src/main/java/controller/PedidosController.java
+++ tutorial-rest-kafka/src/main/java/controller/PedidosController.java
@@ -1,13 +1,22 @@
 package controller;
 
+import data.PedidoData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import services.RegistraEventoService;
 
 @RestController
+@RequiredArgsConstructor
 public class PedidosController {
 
-    public ResponseEntity<String> salvarPedido() {
+    private final RegistraEventoService eventoService;
 
+    @PostMapping(path = "/api/salva-pedido")
+    public ResponseEntity<String> salvarPedido(@RequestBody PedidoData pedido) {
+        eventoService.adicionarEvento("SalvarPedido", pedido);
         return ResponseEntity.ok("Sucesso");
     }
 }
