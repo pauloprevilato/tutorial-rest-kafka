fe1065d0b0e2e23cc5865d458b17cb4cedd11119
Paulo Previlato <paulo.previlato@gmail.com>
2023-10-16 12:25:01 -0400

    Commit initial

---
+++ tutorial-rest-kafka/src/main/java/dio/expertostech/tutorialrestkafka/TutorialRestKafkaApplication.java
@@ -0,0 +1,13 @@
+package dio.expertostech.tutorialrestkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialRestKafkaApplication.class, args);
+	}
+
+}
